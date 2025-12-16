bb4d9c3e7a0104185bc9f44b0a2c9c0b234ea3f0
Petros Asmamaw <petrosasmamaw2@gmail.com>
2025-12-16 20:16:06 +0300

    server started

---
+++ Backend/src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URL,)
+.then(() => console.log('✅ MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
